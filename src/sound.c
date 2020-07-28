ea922defd5214230a2b282113d190ab77f022c48
vamastah <szymorawski@gmail.com>
2020-07-28 23:14:03 +0200

    dynamic sound file extension detection

--- src/sound.c
+++ src/sound.c
@@ -11,34 +11,58 @@ int current_track = 0;
 Mix_Chunk *hit = NULL;
 Mix_Chunk *clr = NULL;
 
+static void *LoadSound(const char *name, int mus);
+
 void initSound(void)
 {
-	int mixflags = MIX_INIT_OGG;
+	int mixflags = MIX_INIT_OGG | MIX_INIT_MOD | MIX_INIT_MP3;
 	if (Mix_Init(mixflags) != mixflags)
 		exit(ERROR_MIXINIT);
 	if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 1, 1024) < 0)
 		exit(ERROR_OPENAUDIO);
-	music[0] = Mix_LoadMUS("sfx/korobeyniki.ogg");
+	music[0] = (Mix_Music*)LoadSound("sfx/korobeyniki", 1);
 	if (!music[0])
 		exit(ERROR_NOSNDFILE);
-	music[1] = Mix_LoadMUS("sfx/bradinsky.ogg");
+	music[1] = (Mix_Music*)LoadSound("sfx/bradinsky", 1);
 	if (!music[1])
 		exit(ERROR_NOSNDFILE);
-	music[2] = Mix_LoadMUS("sfx/kalinka.ogg");
+	music[2] = (Mix_Music*)LoadSound("sfx/kalinka", 1);
 	if (!music[2])
 		exit(ERROR_NOSNDFILE);
-	music[3] = Mix_LoadMUS("sfx/troika.ogg");
+	music[3] = (Mix_Music*)LoadSound("sfx/troika", 1);
 	if (!music[3])
 		exit(ERROR_NOSNDFILE);
-	hit = Mix_LoadWAV("sfx/hit.ogg");
+	hit = (Mix_Chunk*)LoadSound("sfx/hit", 0);
 	if (!hit)
 		exit(ERROR_NOSNDFILE);
-	clr = Mix_LoadWAV("sfx/clear.ogg");
+	clr = (Mix_Chunk*)LoadSound("sfx/clear", 0);
 	if (!clr)
 		exit(ERROR_NOSNDFILE);
 
 	current_track = 0;
 	Mix_VolumeMusic(initmusvol);
+}
+
+static void *LoadSound(const char *name, int mus)
+{
+	static char *ext[] = { ".ogg", ".mod", ".mp3", ".wav" };
+	void *ret = NULL;
+	char buff[256];
+	int len;
+	strcpy(buff, name);
+	len = strlen(buff);
+	for (int i = 0; i < (sizeof(ext)/sizeof(ext[0])); ++i)
+	{
+		buff[len] = '\0';
+		strcat(buff, ext[i]);
+		if (mus)
+			ret = (void*)Mix_LoadMUS(buff);
+		else
+			ret = (void*)Mix_LoadWAV(buff);
+		if (ret)
+			break;
+	}
+	return ret;
 }
 
 void deinitSound(void)
