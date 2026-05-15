3ef785860e8ab442db97a6f84796fdac139201ee
vamastah <szymorawski@gmail.com>
2026-05-16 00:43:47 +0200

    rename lua_example skin to default

--- src/state_mainmenu.c
+++ src/state_mainmenu.c
@@ -120,7 +120,7 @@ static void mainmenu_init_skinload_helper(DIR *dp, const char *maindir)
 	{
 		if (strcmp(ep->d_name, ".") && strcmp(ep->d_name, ".."))
 		{
-			if (!strcmp(ep->d_name, "lua_example"))
+			if (!strcmp(ep->d_name, "default"))
 			menu_skin = menu_skinnum;
 			strcpy(menu_skinentries[menu_skinnum].name, ep->d_name);
 			sprintf(menu_skinentries[menu_skinnum].path, "%s%s/skin.lua", maindir, ep->d_name);
